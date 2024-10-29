3f150639c7ebbd22484a2219e3416355161ba144
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-29 15:51:51 +0530

    new box added with theam proviaded and hover effect

--- material-ui-application/src/components/BoxBasic.jsx
+++ material-ui-application/src/components/BoxBasic.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import { ThemeProvider } from '@mui/material/styles';
 
 /**
  * Basic usage of the Box component.
@@ -10,8 +11,35 @@ import Box from '@mui/material/Box';
 
 export default function BoxBasic() {
   return (
+    <>
     <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
       This Box renders as an HTML section element.
     </Box>
+    <br></br>
+
+    <ThemeProvider
+      theme={{
+        palette: {
+          primary: {
+            main: '#007FFF',
+            dark: 'purple',
+          },
+        },
+      }}
+    >
+      <Box
+        sx={{
+          width: 100,
+          height: 100,
+          borderRadius: 1,
+          border: '1px solid',    
+          bgcolor: 'primary.main',
+          '&:hover': {
+            bgcolor: 'primary.dark',
+          },
+        }}
+      />
+    </ThemeProvider>
+    </>
   );
 }
