a4fa86244c872394215a936171eefba80d78ca8c
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-10-29 16:05:55 +0530

    Stack used for one dimentional layout

--- material-ui-application/src/components/BoxBasic.jsx
+++ material-ui-application/src/components/BoxBasic.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
+import { Stack } from '@mui/material';
 
 /**
  * Basic usage of the Box component.
@@ -11,11 +12,14 @@ import { ThemeProvider } from '@mui/material/styles';
 
 export default function BoxBasic() {
   return (
-    <>
+    <Stack
+    sx={{border: '1px solid', padding: 2}}
+    direction={'row'}
+    spacing={2}>
     <Box component="section" sx={{ p: 2, border: '1px dashed grey' }}>
       This Box renders as an HTML section element.
     </Box>
-    <br></br>
+    
 
     <ThemeProvider
       theme={{
@@ -40,6 +44,6 @@ export default function BoxBasic() {
         }}
       />
     </ThemeProvider>
-    </>
+    </Stack>
   );
 }
