557197821bbbc3b001b32a626cc1a128be5e981c
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-11-08 17:58:30 +0530

    the menubar added

--- material-ui-application/src/components/MuiNavbar.jsx
+++ material-ui-application/src/components/MuiNavbar.jsx
@@ -1,26 +1,61 @@
-import { AppBar,Toolbar, Typography, IconButton, Stack , Button} from '@mui/material';
-import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Stack,
+  Button,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export const MuiNavbar = () => {
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    aria-label="logo"
-                    >
-                    <CatchingPokemonIcon />
-                     </IconButton>
-                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} > Pokemon App </Typography>
-                     <Stack direction="row" spacing={2}>
-                        <Button color="inherit">Features</Button>
-                        <Button color="inherit">Pricing</Button>
-                        <Button color="inherit">About</Button>
-                        <Button color="inherit">Login</Button>
-                     </Stack>
-            </Toolbar>
-        </AppBar>
-    );
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton size="large" edge="start" color="inherit" aria-label="logo">
+          <CatchingPokemonIcon />
+        </IconButton>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {" "}
+          Pokemon App{" "}
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button color="inherit">Features</Button>
+          <Button color="inherit">Pricing</Button>
+          <Button color="inherit">About</Button>
+          <Button
+            color="inherit"
+            id="resource-button"
+            onClick={(e) => setAnchorEl(e.currentTarget)}
+            aria-controls={open ? "resource-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            endIcon={<KeyboardArrowDownIcon />}
+          >
+            Resources
+          </Button>
+          <Button color="inherit">Login</Button>
+        </Stack>
+        <Menu
+          id="resource-menu"
+          anchorEl={anchorEl}
+          open={open}
+          MenuListProps={{ "aria-labelledby": "resource-button" }}
+          onClose={() => setAnchorEl(null)}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          transformOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <MenuItem onClick={() => setAnchorEl(null)}>Blog</MenuItem>
+          <MenuItem onClick={() => setAnchorEl(null)}>Prodcast</MenuItem>
+          <MenuItem onClick={() => setAnchorEl(null)}>Video</MenuItem>
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
 };
