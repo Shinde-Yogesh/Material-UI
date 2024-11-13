ea38a4e40c554ea39ca67267d6b50261137e0cfb
Yogesh Shinde <yogeshshinde2324@gmail.com>
2024-11-13 20:37:39 +0530

    Update MuiLinks.jsx

--- material-ui-application/src/components/MuiLinks.jsx
+++ material-ui-application/src/components/MuiLinks.jsx
@@ -3,6 +3,7 @@ export const MuiLinks = () => {
     return (
         <Stack spacing={2} direction="row" sx={{ p: 2, bgcolor: 'background.paper' }}>
             <Link href="#simple-link">Simple link</Link>    
+{/*             on cursor over link is visiable */}
             <Link href="#" color="inherit">
                 Inherited color link
             </Link>
