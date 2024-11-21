93c08e5b8845b37775784d8184a3679d76a2d61e
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-11-21 18:54:30 +0530

    A breadcrumbs is a list of links that help visualize a page's location within a site's hierarchical structure

---
+++ material-ui-application/src/components/MuiBreadcrumbs.jsx
@@ -0,0 +1,13 @@
+import {Box, Breadcrumbs, Typography} from '@mui/material'
+export const MuiBreadcrumbs = () => {
+    return (
+        <Box m={2}>
+            <Breadcrumbs aria-label="breadcrumb">
+                <a underline="hover" href="#">Home</a>
+                <a underline="hover" href="#">Catalog</a>
+                <a underline="hover" href="#">Accessories</a>
+                <Typography color="text.primary">Shoes</Typography> 
+            </Breadcrumbs>
+        </Box>
+    )
+}
