d5afa52eaf90014fb1428e6860008efbe87681e0
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-11-21 19:20:48 +0530

    adding some props for the importing routing

--- material-ui-application/src/components/MuiBreadcrumbs.jsx
+++ material-ui-application/src/components/MuiBreadcrumbs.jsx
@@ -2,7 +2,9 @@ import {Box, Breadcrumbs, Typography} from '@mui/material'
 export const MuiBreadcrumbs = () => {
     return (
         <Box m={2}>
-            <Breadcrumbs aria-label="breadcrumb">
+            <Breadcrumbs aria-label="breadcrumb" separator=">"
+            maxItems={2}
+            itemsBeforeCollapse={2}>
                 <a underline="hover" href="#">Home</a>
                 <a underline="hover" href="#">Catalog</a>
                 <a underline="hover" href="#">Accessories</a>
