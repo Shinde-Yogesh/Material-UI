680576baadabaf88ae72ee21cbc563181e9d7ddb
Shinde Yogesh <yogeshshinde2324@gmail.com>
2024-11-26 18:59:07 +0530

    added icon while click on Speed dial icon and added onclick event on the speed dial action

--- material-ui-application/src/components/MuiSpeedDial.jsx
+++ material-ui-application/src/components/MuiSpeedDial.jsx
@@ -3,15 +3,17 @@ import {SpeedDial, SpeedDialAction, SpeedDialIcon} from '@mui/material'
 import CopyIcon from '@mui/icons-material/FileCopyOutlined'
 import PrintIcon from '@mui/icons-material/Print'
 import ShareIcon from '@mui/icons-material/Share'
+import EditIcon from '@mui/icons-material/Edit'
+import CloseIcon from '@mui/icons-material/Close'
 
 export const MuiSpeedDial = () => {
   return (
    <SpeedDial ariaLabel='Navigation speed dial'
     sx={{position: 'absolute', bottom: 16, right: 16}}
-     icon={<SpeedDialIcon />}>
-        <SpeedDialAction icon={<CopyIcon />} tooltipTitle='Copy' />
-        <SpeedDialAction icon={<PrintIcon />} tooltipTitle='Print' />
-        <SpeedDialAction icon={<ShareIcon />} tooltipTitle='Share' />
+     icon={<SpeedDialIcon openIcon={<EditIcon />}/>}>
+        <SpeedDialAction icon={<CopyIcon />} tooltipTitle='Copy' onClick={() => console.log('copy')}/>
+        <SpeedDialAction icon={<PrintIcon />} tooltipTitle='Print' onClick={() => console.log('print')}/>
+        <SpeedDialAction icon={<ShareIcon />} tooltipTitle='Share' onClick={() => console.log('share')}/>
    </SpeedDial>
   )
 }
