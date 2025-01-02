1b40d82d6ee2f5ca0c44f48885cb31a0c6b95531
Shinde Yogesh <yogeshshinde2324@gmail.com>
2025-01-02 17:52:56 +0530

    icon and avtar added

--- material-ui-application/src/components/MuiChip.jsx
+++ material-ui-application/src/components/MuiChip.jsx
@@ -1,8 +1,15 @@
-import { Stack,Chip } from '@mui/material'
+import { Stack, Chip, Avatar } from "@mui/material";
+import FaceIcon from "@mui/icons-material/Face";
 export const MuiChip = () => {
   return (
     <Stack spacing={1} direction={"row"}>
-      <Chip label="Chip" color="primary" variant="outlined" />
+      <Chip label="Chip" color="primary" variant="outlined"  icon={<FaceIcon />}/>
+      <Chip
+        label="Chip outlined"
+        color="=secondary"
+        variant="outlined"
+        avatar={<Avatar>V</Avatar>}
+      />
     </Stack>
-  )
-}
+  );
+};
