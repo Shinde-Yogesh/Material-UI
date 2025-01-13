c4afd985007702c5a12e3fe663faffb187108241
Shinde Yogesh <yogeshshinde2324@gmail.com>
2025-01-13 19:56:46 +0530

    delay and leave prop added

--- material-ui-application/src/components/MuiTooltips.jsx
+++ material-ui-application/src/components/MuiTooltips.jsx
@@ -3,7 +3,7 @@ import { Tooltip, IconButton } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 export const MuiTooltips = () => {
   return (
-    <Tooltip title="Delete" arrow>
+    <Tooltip title="Delete" arrow enterDelay={500} leaveDelay={500}>
       <IconButton>
         <DeleteIcon />
       </IconButton>
