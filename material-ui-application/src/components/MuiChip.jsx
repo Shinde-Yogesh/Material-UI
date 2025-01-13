742603de33b01dd6ec906e13d997857ec0f4e1d3
Shinde Yogesh <yogeshshinde2324@gmail.com>
2025-01-13 19:25:09 +0530

    handle chip data by clicking the delete label the chip deleted

--- material-ui-application/src/components/MuiChip.jsx
+++ material-ui-application/src/components/MuiChip.jsx
@@ -1,9 +1,22 @@
 import { Stack, Chip, Avatar } from "@mui/material";
 import FaceIcon from "@mui/icons-material/Face";
+import { useState } from "react";
+
 export const MuiChip = () => {
+  const [chipData, setChipData] = useState(["Chip 1", "Chip 2", "Chip 3"]);
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chipData) =>
+      chipData.filter((chipData) => chipData !== chipToDelete)
+    );
+  };
   return (
     <Stack spacing={1} direction={"row"}>
-      <Chip label="Chip" color="primary" variant="outlined"  icon={<FaceIcon />}/>
+      <Chip
+        label="Chip"
+        color="primary"
+        variant="outlined"
+        icon={<FaceIcon />}
+      />
       <Chip
         label="Chip outlined"
         color="=secondary"
@@ -11,7 +24,19 @@ export const MuiChip = () => {
         avatar={<Avatar>V</Avatar>}
       />
       <Chip label="Click" color="success" onClick={() => alert("Clicked")} />
-    <Chip label="Delete" color="error" onDelete={() => alert("Deleted")} />
+      <Chip label="Delete" color="error" onDelete={() => alert("Deleted")} />
+    {
+      chipData.map((data) => {
+        return (
+          <Chip
+            key={data}
+            label={data}
+            onDelete={handleDelete(data)}
+            color="primary"
+          />
+        );
+      })
+    }
     </Stack>
   );
 };
