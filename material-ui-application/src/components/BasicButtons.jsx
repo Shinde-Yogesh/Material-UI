593e89d282c52f69f17d10626dba373d1b8fa904
Yogesh Shinde <yogeshshinde2324@gmail.com>
2025-01-15 22:19:44 +0530

    Comment added

--- material-ui-application/src/components/BasicButtons.jsx
+++ material-ui-application/src/components/BasicButtons.jsx
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 export default function BasicButtons() {
   return (
     <Stack spacing={2} direction="row">
-      <Button variant="text">Text</Button>
+      <Button variant="text">Text</Button> // Text button created with basic prop
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
     </Stack>
